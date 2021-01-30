b1ad26f596dc1d2cfcfc5c4bed479a2633ea642e
Cheol Ko <cheolko@gmail.com>
2021-01-30 09:59:50 +0900

    2

---
+++ Ch2/main.cpp
@@ -0,0 +1,32 @@
+#ifndef	_CHCH_SDL_
+#define	_CHCH_SDL_	"C:\\_CHCH\\ExternalFiles\\SDL"
+#endif	_CHCH_SDL_ 
+
+#ifdef	_DEBUG  
+#pragma comment(lib, _CHCH_SDL_"\\lib\win\x86\\SDL2.lib") 
+#pragma comment(lib, _CHCH_SDL_"\\lib\win\x86\\SDL2main.lib") 
+#pragma comment(lib, _CHCH_SDL_"\\lib\win\x86\\SDL2.dll") //
+//#else
+//#pragma comment(lib, _CHCH_SDL_"\\build\\x64\\vc15\\lib\\opencv_world451.lib") 
+////#pragma comment(lib, _CHCH_SDL_"\\build\\x64\\vc15\\bin\\opencv_world451.dll")
+#endif		_DEBUG
+
+asd
+f
+sadf
+sa
+dfas
+df
+sadf
+sa
+dfas
+df
+
+
+int main(int argc, char** argv)
+{
+
+
+
+	return 0;
+}
